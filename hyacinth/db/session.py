c4edb8e7657c1c1bcaad619b6e9ea4a65e0a168a
badreldin <71409488+b0dr7@users.noreply.github.com>
2026-06-06 17:00:26 +0300

    Update session.py

--- hyacinth/db/session.py
+++ hyacinth/db/session.py
@@ -1,16 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from hyacinth.db.models import Base
 from hyacinth.settings import get_settings
+import os
 
 settings = get_settings()
-
 credentials = f"{settings.postgres_user}:{settings.postgres_password}"
-host = f"db:5432/{settings.postgres_user}"
+host = f"{os.environ['POSTGRES_HOST']}:{os.environ['POSTGRES_PORT']}/{os.environ['POSTGRES_DB']}"
 connection_string = f"postgresql://{credentials}@{host}"
-
-engine = create_engine(f"postgresql://{credentials}@{host}", future=True)
+engine = create_engine(connection_string, future=True)
 Session = sessionmaker(engine)
-
 Base.metadata.create_all(engine)
